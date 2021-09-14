c362ed8ee395b6fd1e42bb495330dd5ba31f6a76
Ahmed S. Tahir <astahir@gmail.com>
2021-09-14 13:41:54 +0500

    Deployed on rinkeby using hardhat

---
+++ Solidity/scripts/erc1155-deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
+
+    // We get the contract to deploy
+    const ERC1155Practice = await hre.ethers.getContractFactory("ERC1155Practice");
+    const erc1155Practice = await ERC1155Practice.deploy();
+
+    await erc1155Practice.deployed();
+
+    console.log("ERC1155Practice deployed to:", erc1155Practice.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
